6a065bbee4536bd973837d8de5968c5abef9e5d3
Ahmed Sefdine <sefdine668@gmail.com>
2025-04-05 14:30:07 +0000

    Update __init__.py

--- onglets/__init__.py
+++ onglets/__init__.py
@@ -1,5 +1,5 @@
 from .accueil import accueil
 from .analyse import analyse_descriptive
-from .prediction import modelisation
+from .modelisation import modelisation
 from .apropos import a_propos
 from .contact import contact
